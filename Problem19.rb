41ad593664d24447cb1a7f25960c6b4f87e519ac
matsuimai <my.mai521@gmail.com>
2017-03-20 23:08:59 +0900

    Problem19の修正

--- Problem19.rb
+++ Problem19.rb
@@ -1,6 +1,6 @@
 # 月=1 火=2 水=3 木=4 金=5 土=6 日=0
 def getDays(month,year)
-    if month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 then
+    if month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12 then
 	return 31
     elsif month == 4 || month == 6 || month == 9 || month == 11 then
         return 30
@@ -21,17 +21,22 @@ end
 
 
 def main()
-    startYear = 2001
-    lastYear = 2002
+    startYear = 1900
+    lastYear = 2000
     date = 1
+    count = 0
 
     for year in startYear..lastYear do
 	for month in 1..12 do
 	    date = nextDate(date, month, year)
-	    print("month: ", month, " date: ", date)
+	    #print("month: ", month, " date: ", date)
+	    
+	    if year != 1900 && date == 0 then
+	       count = count + 1
+	    end
 	end
     end
-    return date
+    return count
 end
 
 print(main())
