8b98e67c00472758a85434ece7e85f0ee117ebfc
matsuimai <my.mai521@gmail.com>
2017-03-25 19:46:51 +0900

    Problem22 各配列の単語のアルファベットのスコアを合計する

--- Problem22.rb
+++ Problem22.rb
@@ -1,6 +1,11 @@
 def countScore
     dataList = getData("names.txt", "r")
-    print(dataList)
+    score = dataList.inject(0) do |count, m|
+        count = count + countLetter(m)
+	count
+    end
+    
+    print(score)
 end
 
 
@@ -15,6 +20,19 @@ def getData(filename,mode)
     end
     return dataList
 end
+
+def countLetter(str)
+    alphabet = { "A"=>1, "B"=>2, "C"=>3, "D"=>4, "E"=>5, "F"=>6, "G"=>7, "H"=>8, "I"=>9, "J"=>10, "K"=>11, "L"=>12, "M"=>13, "N"=>14, 
+    	       	 "O"=>15, "P"=>16, "Q"=>17, "R"=>18, "S"=>19, "T"=>20, "U"=>21, "V"=>22, "W"=>23, "X"=>24, "Y"=>25, "Z"=>26 }
+
+    letter = str.split("")
+    score = letter.inject(0) do |count, n|
+    	count = count + alphabet[n]
+	count
+    end
+    return score
+end
+
 
 
 countScore
