f87c1af0171b37e7ee82a7fd9f7c3e091aa72e9c
matsuimai <my.mai521@gmail.com>
2017-03-30 00:45:48 +0900

    Problem26 回答完成

--- Problem26.rb
+++ Problem26.rb
@@ -11,4 +11,18 @@ def cycleCalc(num)
    end
 end
 
-print(cycleCalc(3))
+
+def searchMax(upper)
+    maxCount = 1
+    answer = 0
+    for num in 1..upper do
+    	if maxCount < cycleCalc(num) then
+	   maxCount = cycleCalc(num)
+	   answer = num
+	end
+    end
+    return answer
+end
+
+
+print(searchMax(1000))
