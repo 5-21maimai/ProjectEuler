7372c037cf0ec6daa522546e2861d0b568ca039e
matsuimai <my.mai521@gmail.com>
2017-04-01 01:24:50 +0900

    Problem27 途中

--- Problem27.rb
+++ Problem27.rb
@@ -7,3 +7,32 @@ def checkPrime(num)
     return true
 end
 
+def createPrimeList(max)
+    primeList = []
+    for i in 2..max do
+    	if checkPrime(i) then
+	    primeList << i
+	end
+    end
+    return primeList
+end
+
+
+def calc(n,a,b)
+    result = n ** 2 + a * n + b
+    if checkPrime(result) then
+        return true
+    end
+    return false
+end
+
+
+def main()
+    bList = createPrimeList(1000)
+end
+
+
+
+
+
+print(createPrimeList(13))
