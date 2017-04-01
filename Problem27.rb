362c1a2fcaac405ccff7a348fb595dd50085c024
matsuimai <my.mai521@gmail.com>
2017-04-01 12:49:49 +0900

    Problem27 回答完成

--- Problem27.rb
+++ Problem27.rb
@@ -19,20 +19,51 @@ end
 
 
 def calc(n,a,b)
-    result = n ** 2 + a * n + b
-    if checkPrime(result) then
-        return true
+    result = n * n + a * n + b
+    if result > 0 && checkPrime(result) then
+	return true
     end
     return false
 end
 
 
-def main()
-    bList = createPrimeList(1000)
+def countPrime(a,b)
+    n = 0
+    while 0 < 1 do
+    	if calc(n,a,b)
+	    n = n + 1
+	else
+	    return n - 1
+	end
+    end
+    return 0
 end
 
 
+def main()
+    bList = createPrimeList(1000)
+    maxPrime = 0
+    mulResult = 0    
 
+    for a in 0..999 do
+    	for b in bList do
+    	    countPrime = countPrime(a,b)
+	    if countPrime > maxPrime
+	       maxPrime = countPrime
+	       mulResult = a * b
+	    end
+	    
+	    minusA = 0 - a
+	    countPrime = countPrime(minusA,b)
+	    if countPrime > maxPrime
+               maxPrime	  = countPrime
+	       mulResult = minusA * b
+	    end
 
+	end
+    end
+    print(mulResult)
 
-print(createPrimeList(13))
+end
+
+main()
