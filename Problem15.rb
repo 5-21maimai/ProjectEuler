17e4023130805483fd10ba7f5976bf2d449a67cc
matsuimai <my.mai521@gmail.com>
2017-04-08 04:33:57 +0900

    Problem15 Problem16 修正

--- Problem15.rb
+++ Problem15.rb
@@ -1,11 +1,11 @@
-def main(a,b)
+def countRoot(a,b)
     num = a + b
     answer = 1
     while num > a
-       answer = answer * (num / (num - a))
+       answer = answer * (num.to_f / (num - a).to_f)
        num = num - 1
     end
-    print(answer)
+    print(answer.to_i)
 end
 
-main(3,2)
+countRoot(20,20)
--- Problem16.rb
+++ Problem16.rb
@@ -1,13 +1,12 @@
-def SumEachFigure(power)
+def SumEachDigit(power)
     sum = 0
-    result = 2 ** power
-    resultStr = result.to_s
+    resultStr = (2 ** power).to_s
     figureList = resultStr.split("")
     
-    for i in figureList do
+    figureList.each do |i|
         sum = sum + i.to_i
     end
     return sum
 end
 
-print(SumEachFigure(1000))
+print(SumEachDigit(1000))
