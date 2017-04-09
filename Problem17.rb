eb054ee9d6ef5c8b668de32a57c17f23d5773533
matsuimai <my.mai521@gmail.com>
2017-04-09 20:33:58 +0900

    Problem17 修正

--- Problem17.rb
+++ Problem17.rb
@@ -1,60 +1,41 @@
-@englishList = { 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten",
-    		 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen",
-		 18 => "eighteen",19 => "nineteen", 20 => "twenty", 30 => "thirty", 40 => "forty", 50 => "fifty", 60 => "sixty", 70 => "seventy",
-		 80 => "eighty", 90 => "ninety" }
-
-
 def countWord(max)
     count = 0
     for num in 1..max do
-    	word = createWord(num)
-	count = count + word.length
+	count = count + createWord(num).length
     end
     return count
 end
 
-
 def createWord(num)
-    if num == 1000
-       return "onethousand"
-    end
-    
-    word = ""
-    hundreds = num / 100
-    if hundreds != 0 then
-       word = word + @englishList[hundreds] + "hundred"
-    end
+    englishList = { 0 => "", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 
+    		 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen",
+                 18 => "eighteen",19 => "nineteen", 20 => "twenty", 30 => "thirty", 40 => "forty", 50 => "fifty", 60 => "sixty", 70 => "seventy",
+                 80 => "eighty", 90 => "ninety" }
 
+    return "onethousand" if num == 1000
+
+    return englishList[num] if num < 20
+
+    return englishList[(num / 10) * 10] + englishList[num % 10] if num >= 20 && num < 100
+
+    return englishList[num / 100] + "hundred" if num % 100  == 0
+
+    word = englishList[num / 100] + "hundred"
     hundredsRemainder = num % 100
-    if hundredsRemainder == 0 then
-       return word
-    end
-
     if hundredsRemainder < 20 then
-       if hundreds == 0 then
-       	  word = word + @englishList[hundredsRemainder]
-       else
-          word = word + "and" + @englishList[hundredsRemainder] 
-       end
+       word = word + "and" + englishList[hundredsRemainder] 
        return word
     end
        
-    tens = hundredsRemainder / 10
-    if hundreds == 0 then
-        word = word + @englishList[tens * 10]
-    else
-        word = word + "and" + @englishList[tens * 10]
-    end
+    word = word + "and" + englishList[(hundredsRemainder / 10) * 10]
 
     tensRemainder = hundredsRemainder % 10
     if tensRemainder != 0 then
-       word = word + @englishList[tensRemainder]
+       word = word + englishList[tensRemainder]
     end
 
     return word
-
 end
-
 
 print(countWord(1000))
        
