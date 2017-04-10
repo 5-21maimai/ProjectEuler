ec51f1debc7ccce0ab2cf91a3f759b90cce2e481
matsuimai <my.mai521@gmail.com>
2017-04-11 00:08:05 +0900

    Problem19 修正

--- Problem19.rb
+++ Problem19.rb
@@ -1,41 +1,19 @@
-# 月=1 火=2 水=3 木=4 金=5 土=6 日=0
-def getDays(month,year)
-    if month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12 then
-	return 31
-    elsif month == 4 || month == 6 || month == 9 || month == 11 then
-        return 30
-    elsif year % 400 == 0 then
-    	return 29
-    elsif year % 100 == 0 then
-    	return 28
-    elsif year % 4 == 0 then
-    	return 29
-    else
-	return 28
-    end
-end
+require 'date'
 
-def nextDate(date, month, year)
-    return (date + getDays(month, year)) % 7
-end
-
+# 月=1 火=2 水=3 木=4 金=5 土=6 日=7
 
 def main()
-    startYear = 1900
-    lastYear = 2000
-    date = 1
+    years = (1901..2000)
+    months = (1..12)
     count = 0
 
-    for year in startYear..lastYear do
-	for month in 1..12 do
-	    date = nextDate(date, month, year)
-	    #print("month: ", month, " date: ", date)
-	    
-	    if year != 1900 && date == 0 then
-	       count = count + 1
-	    end
-	end
-    end
+    years.each do |y|
+    	 months.each do |m|
+	     if Date.new(y, m, 1).cwday == 7 then
+	      	 count = count + 1
+	     end
+	 end
+    end   
     return count
 end
 
