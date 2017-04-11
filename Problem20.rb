db6683e7f39e15ffe2b29c083cd769ac9793438d
matsuimai <my.mai521@gmail.com>
2017-04-12 00:50:03 +0900

    Problem20 修正

--- Problem20.rb
+++ Problem20.rb
@@ -1,20 +1,13 @@
 def multiplication(num)
-    mul = 1
-    for i in 1...num do
-        mul = mul * i
-    end
-    
+    mul = (1..num).inject(1) {|mul, n| mul * n }
     result = sumEachFigure(mul)
     return result
 end
 
 def sumEachFigure(mul)
-   sum = 0
-    mulStr = mul.to_s
-    
-    figureList = mulStr.split("")
-    
-    for i in figureList do
+    sum = 0
+    figureList = mul.to_s.split("")
+    figureList.each do |i|
         sum = sum + i.to_i
     end
     return sum
