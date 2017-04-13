33830308ef0df1af9de36a5a2938ec1f0a02a09c
matsuimai <my.mai521@gmail.com>
2017-04-13 23:48:48 +0900

    Problem22 修正

--- Problem22.rb
+++ Problem22.rb
@@ -1,10 +1,8 @@
-def countScore
-    dataList = getData("names.txt", "r")
+def countScore()
+    dataList = getData("names22.txt", "r")
     score = dataList.inject(0) do |count, m|
-	count = count + (dataList.index(m)+1) * countLetter(m)
-	count
-    end
-    
+	count + (dataList.index(m)+1) * countLetter(m)
+    end    
     print(score)
 end
 
@@ -13,24 +11,18 @@ def getData(filename,mode)
     dataList = []
     File.open(filename,mode) do |f|
         f.each_line do |data|
-	    data = data.gsub("\"","")
-      	    dataList = data.split(",")
-	    dataList.sort!
+	    data = data.gsub('"','')
+      	    dataList = data.split(',').sort!
         end
     end
     return dataList
 end
 
 def countLetter(str)
-    alphabet = { "A"=>1, "B"=>2, "C"=>3, "D"=>4, "E"=>5, "F"=>6, "G"=>7, "H"=>8, "I"=>9, "J"=>10, "K"=>11, "L"=>12, "M"=>13, "N"=>14, 
-    	       	 "O"=>15, "P"=>16, "Q"=>17, "R"=>18, "S"=>19, "T"=>20, "U"=>21, "V"=>22, "W"=>23, "X"=>24, "Y"=>25, "Z"=>26 }
-
-    letter = str.split("")
-    score = letter.inject(0) do |count, n|
-    	count = count + alphabet[n]
-	count
+    score = str.split("").inject(0) do |count, n|
+    	count + (n.ord - 'A'.ord + 1) 
     end
     return score
 end
 
-countScore
+countScore()
