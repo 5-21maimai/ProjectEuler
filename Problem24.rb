310bb1bae9e25f85124baa6b40a9201e5820cefb
matsuimai <my.mai521@gmail.com>
2017-04-15 22:43:03 +0900

    Problem24 修正

--- Problem24.rb
+++ Problem24.rb
@@ -1,12 +1,9 @@
 def getPermutation(num)
     count = 1
     answer = ""
-    numberList = [0,1,2,3,4,5,6,7,8,9]
-    numberList.permutation(10) do |i|
+    (0..9).to_a.permutation(10) do |arr|
     	if count == num then
-	    for item in i do
-		answer = answer + item.to_s
-	    end
+	    arr.each { |i| answer = answer + i.to_s }
 	    return answer
 	end
 	count = count + 1
