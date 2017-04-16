abfeb87053e06aa6f99043d6497dac487912c618
matsuimai <my.mai521@gmail.com>
2017-04-16 19:07:48 +0900

    Problem25 修正

--- Problem25.rb
+++ Problem25.rb
@@ -2,22 +2,17 @@ def calcFibonacci()
     count = 3
     a = 1
     b = 1
-    while 0 < 1 do
+    while true do
     	 sum = a + b
-    	 digit = checkDigit(sum)
-	 if digit == 1000
-	     return count 
-	 end
+	 return count if checkDigit(sum) == 1000
 	 b = a
 	 a = sum
 	 count = count + 1
     end
 end
 
-
 def checkDigit(num)
-    numstr = num.to_s
-    return numstr.split("").length
+    return num.to_s.split("").length
 end
 
 
