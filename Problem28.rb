3b123c3cafb72718f2934aa08cf8aba3bb6cb99f
matsuimai <my.mai521@gmail.com>
2017-04-20 07:19:49 +0900

    Problem28 修正途中

--- Problem28.rb
+++ Problem28.rb
@@ -1,15 +1,16 @@
 def calcSide(length)
-    upperRight = length ** 2
-    upperLeft = upperRight - (length - 1)
-    lowerLeft = upperLeft - (length - 1)
-    lowerRight = lowerLeft - (length - 1)
-    sum = upperRight + upperLeft + lowerLeft + lowerRight
+    sum = 0
+    target = length ** 2
+    for j in 1..4 do 
+        sum = sum + target
+	target = target - (length - 1)
+    end
     return sum
 end
 
 def main()
     result = 0
-    for i in 1..1001 do
+    (1..1001).each do |i|
     	if i == 1 then
 	    result = result + 1
 	elsif i % 2 != 0 then
