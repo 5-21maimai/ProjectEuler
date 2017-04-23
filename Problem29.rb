3b0b606a6848287612739676beeea906687c5640
matsuimai <my.mai521@gmail.com>
2017-04-23 13:35:33 +0900

    Problem29 修正

--- Problem29.rb
+++ Problem29.rb
@@ -1,21 +1,12 @@
-def calcPower(a,b)
-    power = a ** b
-    return power
-end
-
-def main()
+def calcPower()
     powerList = []
-    for a in 2..100
-    	for b in 2..100
-	    power = calcPower(a,b)
-	    if powerList.include?(power) == false then
-	         powerList << power
-	    end
+    2.upto(100) do |a|
+    	2.upto(100) do |b|
+	    powerList <<  a ** b
 	end
     end
-    
-    print(powerList.length)
+    print(powerList.uniq.length)
 end
 
-main()
+calcPower()
 	
