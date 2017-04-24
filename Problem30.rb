69fff79238ea0da17c4d93b9521e0b7b367d9b2a
matsuimai <my.mai521@gmail.com>
2017-04-25 00:39:45 +0900

    Problem30 修正

--- Problem30.rb
+++ Problem30.rb
@@ -1,22 +1,17 @@
 def sumEachPlace(num)
     eachPlaceList = num.to_s.split("")
-    sum = 0
-    for place in eachPlaceList do
-    	sum = sum + place.to_i ** 5
-    end
-    return sum
+    return eachPlaceList.inject(0){ |sum, place| sum + place.to_i ** 5 }
 end
 
 def main()
     matchNumList = []
     2.upto(999999) do |num|
-    	sum = sumEachPlace(num)
-	if num == sum then
+	if num == sumEachPlace(num) then
 	    matchNumList << num
 	end
     end
-    matchNumList.inject(0){ |result, i| result + i }
-    print(result)
+    print(matchNumList.inject(0){ |result, i| result + i })
+
 end
 
 
