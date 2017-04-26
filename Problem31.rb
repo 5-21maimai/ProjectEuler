33c46577386b0e13552f7107da90bff7a98018d3
matsuimai <my.mai521@gmail.com>
2017-04-27 07:03:28 +0900

    Problem31 途中

---
+++ Problem31.rb
@@ -0,0 +1,13 @@
+def countCoins(target, coins)
+    if coins.length == 0
+       return 1
+
+    
+
+
+end
+
+def main()
+    coins = [1, 2, 5, 10, 20, 50, 100, 200]
+    target = 200
+    countCoins(target,coins)
