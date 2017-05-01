44df919763fdc2e5feec9cdf30805a1062e3390a
matsuimai <my.mai521@gmail.com>
2017-05-01 15:43:18 +0900

    Problem31 途中3 答えが違う・・・

--- Problem31.rb
+++ Problem31.rb
@@ -1,18 +1,21 @@
 def countCoins(target, coins)
+    if coins.length == 0 then
+        return 1
+    end
     sum = 0
-    coin = coin[0]
+    coin = coins[0]
     selectLimit = target / coin
     0.upto(selectLimit) do |i|
-    	 s = s + countCoins(target / i , coins)
+    	subCoins = coins[1..coins.length-1]
+        sum = sum + countCoins(target - coin * i , subCoins)
     end
-
-    print(s)
-
-
+    return sum
 end
 
 def main()
     coins = [200, 100, 50, 20, 10, 5, 2, 1]
     target = 200
-    countCoins(target,coins)
+    print(countCoins(target,coins))
 end
+
+main()
