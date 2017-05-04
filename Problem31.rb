dd15baeb541c0cfe52a43c96b982bdf805611588
matsuimai <my.mai521@gmail.com>
2017-05-04 22:52:53 +0900

    Problem31 回答

--- Problem31.rb
+++ Problem31.rb
@@ -1,17 +1,16 @@
-def countCoins(target, coins, lastCoin = 0)
+def countCoins target, coins, lastCoin=0
     return 1 if target == 0
     total = 0
-
     coins.each do |c|
     	next if c < lastCoin
-	total += countCoins(coins, target - c, c) if (target >= c)
+	total += countCoins(target - c, coins, c) if (target >= c)
     end
-    return total
+    total
 
 end
 
 def main()
-    coins = [200, 100, 50, 20, 10, 5, 2, 1]
+    coins = [1,2,5,10,20,50,100]
     target = 200
     print(countCoins(target,coins))
 end
