0c1b5005e5c1828da5776950916067a9efce89b6
matsuimai <my.mai521@gmail.com>
2017-05-06 23:54:09 +0900

    Problem32 途中2

--- Problem32.rb
+++ Problem32.rb
@@ -1,17 +1,28 @@
 def createPandigital()
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    total = 0
     numbers.permutation(9) {|arr|
-        print(arr)
+        total = total + checkProduct(arr)
     }
-
+    return total
 end
 
 
 def checkProduct(arr)
-    3arr = arr[0..2]
-    2arr = arr[3..4]
-    productArr = arr[5..8]
+    threeInteger = arr[0..2].join(", ").to_i
+    twoInteger = arr[3..4].join(", ").to_i
+    fourInteger = arr[0..3].join(", ").to_i
+    oneInteger = arr[0]
+    productInteger = arr[5..8].join(", ").to_i
+
+    if(threeInteger * twoInteger == productInteger) then
+        return productInteger
+    elsif(fourInteger * oneInteger == productInteger) then
+        return productInteger
+    else
+	return 0
+    end
 
 end
 
-createPandigital()
+print(createPandigital())
