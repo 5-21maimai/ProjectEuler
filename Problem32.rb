42813456087fa08f067eb1c7aaebaaef58cb4449
matsuimai <my.mai521@gmail.com>
2017-05-07 23:07:40 +0900

    Problem32 回答

--- Problem32.rb
+++ Problem32.rb
@@ -1,28 +1,34 @@
 def createPandigital()
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     total = 0
+    cacheList = []
     numbers.permutation(9) {|arr|
-        total = total + checkProduct(arr)
+        mul = checkProduct(arr)
+        if cacheList.include?(mul)==false then
+	    cacheList << mul
+	    total = total + mul
+	end
     }
     return total
 end
 
 
 def checkProduct(arr)
-    threeInteger = arr[0..2].join(", ").to_i
-    twoInteger = arr[3..4].join(", ").to_i
-    fourInteger = arr[0..3].join(", ").to_i
-    oneInteger = arr[0]
-    productInteger = arr[5..8].join(", ").to_i
+    threeInteger = arr[0..2].join.to_i
+    twoInteger = arr[3..4].join.to_i
+    fourInteger = arr[0..3].join.to_i
+    oneInteger = arr[4]
+    productInteger = arr[5..8].join.to_i
 
     if(threeInteger * twoInteger == productInteger) then
-        return productInteger
+	return productInteger
     elsif(fourInteger * oneInteger == productInteger) then
-        return productInteger
+	return productInteger
     else
 	return 0
     end
 
 end
+
 
 print(createPandigital())
