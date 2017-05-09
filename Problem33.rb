a094035cbbd5ef1895b64f9badcb451a583891e9
matsuimai <my.mai521@gmail.com>
2017-05-10 00:25:12 +0900

    Problem33 あとちょっと

--- Problem33.rb
+++ Problem33.rb
@@ -4,10 +4,28 @@ def checkSameDigit(n,d)
     leftD = d / 10
     rightD = d % 10
 
-    if rightN != leftD || rightD == 0
-       return false
+    if rightD == 0
+        return false
+    elsif leftD == rightN
+        return leftN/rightD.to_f == n/d.to_f
+    elsif rightD == leftN
+    	return rightN/leftD.to_f == n/d.to_f
+    elsif rightD == rightN
+    	return leftN/leftD.to_f == n/d.to_f
+    else
+        return false
     end
-
-    return true
-
 end
+
+
+def main()
+    (10..98).each do |n|
+        (n+1 .. 99).each do |d|
+	    if checkSameDigit(n,d)
+	     	print(n,d,"\n")
+	    end
+	end
+    end
+end
+
+main()
