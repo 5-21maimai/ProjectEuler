f3a75eef60fcad9e7867ed8177db81a298a8215c
matsuimai <my.mai521@gmail.com>
2017-05-16 22:54:06 +0900

    Problem35 回答

--- Problem35.rb
+++ Problem35.rb
@@ -1,5 +1,5 @@
 def checkPrime(num)
-    (2..num/2).each do |n|
+    (2..num-1).each do |n|
         if num % n == 0 then
 	    return false
         end
@@ -7,26 +7,32 @@ def checkPrime(num)
     return true
 end
 
+def checkDigit(eachDigit)
+    if eachDigit.include?("2") || eachDigit.include?("4") || eachDigit.include?("5") ||
+       eachDigit.include?("6") || eachDigit.include?("8") || eachDigit.include?("0") then
+        return false    
+    end
+
+    eachDigit.length.times do
+        if checkPrime(eachDigit.join.to_i) == false
+            return false
+        end
+	eachDigit.rotate!(1)
+    end
+    return true
+end
+
+
+
 def main()
-    count = 0
-    (2..1000000).each do |num|        
+    count = 3
+    (6..1000000).each do |num|        
         checkFlag = true
         eachDigit = num.to_s.split("")
-	if eachDigit.include?("2") || eachDigit.include?("4") || eachDigit.include?("5") || 
-	    eachDigit.include?("6") || eachDigit.include?("8") || eachDigit.include?("0") then
-	    next
-        end
-	
-	eachDigit.permutation() {|arr|
-	    if checkPrime(arr.join.to_i) == false
-	        checkFlag = false
-		next
-            end
-        }
 
-	if checkFlag then
-	    count = count + 1
-	    print(count,num,"\n")	
+	if checkDigit(eachDigit) then
+	    print(num,"\n")
+	    count = count + 1	
         end
     end
     print(count,"\n")
