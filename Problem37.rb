2c79c6884e213e1824e0dc874a615108b2ccf70d
matsuimai <my.mai521@gmail.com>
2017-05-22 01:18:15 +0900

    Problem37 途中2

--- Problem37.rb
+++ Problem37.rb
@@ -1,4 +1,5 @@
-def checkPrime(num)
+def checkPrime(stringNum)
+    num = stringNum.to_i
     (2..num/2).each do |i|
         if num % i == 0
 	    return false
@@ -7,14 +8,30 @@ def checkPrime(num)
     return true
 end
 
+def calcSlice(num)
+    snum = num.to_s
+    while snum != ""
+    	if checkPrime(snum) == false
+	    return false
+	end
+	snum.slice!(0)
+    end
+    return true
+end
+
 
 def main()
+    sum = 0
     (8..100).each do |num|
-        print(sum.to_s.slice!(0),"\n")
-
+	if calcSlice(num) == true
+	    sum = sum + num
+	end
     end
+    print(sum)
 
 end
+
+
 
 
 main()
