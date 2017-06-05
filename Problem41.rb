9514ab5be5cbe2649d9bac6a24b671d3f6ebded4
matsuimai <my.mai521@gmail.com>
2017-06-05 23:33:15 +0900

    Problem41 途中

--- Problem41.rb
+++ Problem41.rb
@@ -1,11 +1,19 @@
-def createPanDigital()
+def main()
     panList = []
-    (1..9).to_a.permutation(9) do |arr|
-        panList << arr.join.to_i
+    answer = 0
+    7.downto(1).to_a.permutation(7) do |arr|
+        panNum = arr.join.to_i
+	if checkPrime(panNum)
+	    answer = panNum
+	    break
+	end
     end
-    print(panList)
-
+    print(answer)
 end
 
+def checkPrime(num)
+    (2..num/2).each{|i| return false if num % i == 0}
+    return true
+end
 
-createPanDigital()
+main()
