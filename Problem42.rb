69f5b4fa6e95a8efa741a226e25a111cfc3f62e3
matsuimai <my.mai521@gmail.com>
2017-06-08 01:44:44 +0900

    Problem42 あとちょっと

--- Problem42.rb
+++ Problem42.rb
@@ -10,4 +10,32 @@ def getData()
 end
 
 
-getData()
+def getSankaku()
+    sankakuList = []
+
+    return sankakuList
+end
+
+
+def main()
+    dataList = getData()
+    sankakuList = getSankaku()
+    count = 0
+    dataList.each do |str|
+        score = countLetter(str)
+	count = count + 1 if sankakuList.include?(score)
+    end
+    print(count)
+end
+
+
+def countLetter(str)
+    score = str.split("").inject(0) do |count, n|
+    	  count + (n.ord - 'A'.ord + 1) 
+    end
+    return score
+end
+
+
+
+main()
