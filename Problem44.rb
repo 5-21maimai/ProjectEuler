b2a1a20a54d574aedff71118441483daad24fec9
matsuimai <my.mai521@gmail.com>
2017-06-13 23:26:53 +0900

    Problem44 途中 ちょっと難しい・・・

--- Problem44.rb
+++ Problem44.rb
@@ -1,14 +1,42 @@
-def createPentagonalNumber()
+def createPentagonalNumber(max)
     i = 0
     pentagonalList = []
-    while true
+    while i < max
     	calc = i * (3 * i - 1) / 2
-        pentagonalList.unshift(calc)
-	print(pentagonalList)
+        pentagonalList << calc
 	i = i + 1
     end
+    return pentagonalList
+end
+
+
+def checkPentagon(max, pList)
+    min = pList[max-1].to_i
+    (1..max).each do |i|
+        (i+1..max).each do |j|
+            sum = pList[i].to_i + pList[j].to_i
+	    sub = pList[j].to_i - pList[i].to_i
+	    if pList.include?(sum) && pList.include?(sub)
+	        print(sub,"\n")
+	        if sub < min then
+		    min = sub
+		    print(min,"\n")		   
+		end
+	    end
+	end
+    end
+    return min
+end
+
+
+def main()
+    max = 2000
+    pList = createPentagonalNumber(max)
+    print(pList)
+    subMin = checkPentagon(max,pList)
+    print(subMin)
 
 end
 
 
-createPentagonalNumber()
+main()
