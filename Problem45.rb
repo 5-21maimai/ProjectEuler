1df8389e009a5b6630cb1173125517030f5c13d4
matsuimai <my.mai521@gmail.com>
2017-06-16 21:22:13 +0900

    Problem45 回答

--- Problem45.rb
+++ Problem45.rb
@@ -2,13 +2,24 @@ def createHexagon(n)
     return n * (2 * n - 1)
 end
 
+def createPentagon(n)
+    return n * (3 * n - 1) / 2
+
+end
+
 
 def main()
-    144.upto(200) do |n|
-        hexagon = createHexagon(n)
-	print(hexagon,"\n")
+    hexagonList = []
+    answer = 0
+    144.upto(100000) do |n|
+        hexagonList << createHexagon(n)
+	pentagon = createPentagon(n)
+	if hexagonList.include?(pentagon) then
+	    answer = pentagon
+	    break
+	end
     end
-
+    print(answer)
 end
 
 
