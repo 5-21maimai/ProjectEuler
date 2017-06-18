918d726eeb07f5e4f237686fde48a8551f151629
matsuimai <my.mai521@gmail.com>
2017-06-18 23:36:19 +0900

    Problem46 回答

--- Problem46.rb
+++ Problem46.rb
@@ -1,6 +1,7 @@
 # 奇合成数とは：1より大きい整数であって，素数でない奇数
 
 def isPrime(num)
+    return false if num == 1
     (2..num/2).each do |i|
         if num % i == 0
 	    return false
@@ -11,24 +12,30 @@ end
 
 
 def isExpected(synthesisNum)
-    i = 1
+    i = 0
     while true
+    	i = i + 1
         doubleSquare = 2 * i ** 2
-	if synthesisNum > doubleSquare then
-	    if isPrime(synthesisNum - doubleSquare) then
-	        return true
-	    else
-	        next
-	    end
-	else
-	    break
-	end
-	i = i + 1
-    end
+	break if doubleSquare > synthesisNum
 
+	if isPrime(synthesisNum - doubleSquare) then
+	    return true
+	else
+	    next
+	end
+    end
     return false
+end
+
+def main()
+    num = 33
+    while true
+        num = num + 2
+	next if isPrime(num) == true
+	break if isExpected(num) == false
+    end
+    print(num)
 
 end
 
-
-print(inExpected(9))
+main()
