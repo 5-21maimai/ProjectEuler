acd54f0927af32a70685b1e2fa24c1e04aba8278
matsuimai <my.mai521@gmail.com>
2017-06-22 05:35:29 +0900

    Problem47 回答

--- Problem47.rb
+++ Problem47.rb
@@ -8,12 +8,25 @@ end
 
 def main()
     (1..1000000).each do |num|
-        PrimeList = calcPrimeDivision(num)
+        flag = true
+	n = num
+        4.times do |first|
+	    primeDivisionList = calcPrimeDivision(n)
+	    if primeDivisionList.size() != 4
+	        flag = false
+		break
+	    end
+	    n = n + 1
+	end
+	
+	if flag
+	    print(num,"\n")
+	    break
+	end
+
     end
 
 end
 
-
-print(calcPrimeDivision(12))
 
 main()
