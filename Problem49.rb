ff2da923752a2631a564657e8d698224e5cd6d0a
matsuimai <my.mai521@gmail.com>
2017-06-25 22:25:03 +0900

    Problem49 途中

--- Problem49.rb
+++ Problem49.rb
@@ -5,9 +5,22 @@ def main()
     (1000..9999).each do |i|
         primeList << i if Prime.prime?(i)
     end
+    
+    (1..primeList.size()-1).each do |a|
+        (a+1..primeList.size()).each do |b|
+	    firstNumber = primeList[a-1]
+	    secondNumber = primeList[b-1]
+	    thirdNumber = secondNumber + secondNumber - firstNumber
 
-    print(primeList)
+	    if primeList.include?(thirdNumber) then
+	        # sortして３つの数が一致するか調べるコード
+	    end
+	end
+    end
+    
 end
+
+
 
 
 main()
