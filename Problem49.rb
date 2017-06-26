ded3a2f0114eca7cc0b6529055c50b63f00e9863
matsuimai <my.mai521@gmail.com>
2017-06-26 23:16:48 +0900

    Problem49 回答

--- Problem49.rb
+++ Problem49.rb
@@ -1,10 +1,7 @@
 require 'prime'
 
 def main()
-    primeList = []
-    (1000..9999).each do |i|
-        primeList << i if Prime.prime?(i)
-    end
+    primeList = createPrimeList
     
     (1..primeList.size()-1).each do |a|
         (a+1..primeList.size()).each do |b|
@@ -13,11 +10,36 @@ def main()
 	    thirdNumber = secondNumber + secondNumber - firstNumber
 
 	    if primeList.include?(thirdNumber) then
-	        # sortして３つの数が一致するか調べるコード
+	        if isSameDigit(firstNumber,secondNumber,thirdNumber)
+		    print(firstNumber,secondNumber,thirdNumber)
+		end
 	    end
 	end
     end
     
+end
+
+def createPrimeList()
+    primeList = []
+    (1000..9999).each do |i|
+        primeList << i if Prime.prime?(i)
+    end
+    return primeList
+end
+
+
+def isSameDigit(firstNumber,secondNumber,thirdNumber)
+    firstDigit = firstNumber.to_s.split("")
+    secondDigit = secondNumber.to_s.split("")
+    thirdDigit = thirdNumber.to_s.split("")
+    if firstDigit.sort != secondDigit.sort
+	return false
+    end
+
+    if secondDigit.sort != thirdDigit.sort
+        return false
+    end
+    return true
 end
 
 
