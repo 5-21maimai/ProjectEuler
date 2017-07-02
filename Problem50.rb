968953041bbca2cb3ebd5cd2acb264589671cbac
matsuimai <my.mai521@gmail.com>
2017-07-02 16:33:26 +0900

    Problem50 回答 分からなくてカンニングしました

--- Problem50.rb
+++ Problem50.rb
@@ -1,29 +1,25 @@
-require 'prime'
-
-def main()
-    provisionalSum = 0
-    sum = 0
-    Prime.each(100).each do |prime|
-    	print(prime,"\n")
-        provisionalSum = provisionalSum + prime
-	#print(provisionalSum,"\n")
-
-	if provisionalSum.prime?
-	    sum = provisionalSum
-	    #print(sum,"\n")
-	    next
-	end
-	
-	#print(provisionalSum,"\n")
-	if provisionalSum >= 1000 then
-	    break
-	end
-	
-    end
-
-    print(sum)
-
+max = 1000000
+prime = Array.new(max, 1)
+prime[0..1] = [0, 0]
+i = 2
+while i < Math.sqrt(max).to_i + 1 do
+      (i + i).step(max, i){|x| prime[x] = 0}
+      i += prime[(i + 1)..max].index(1) + 1
 end
 
-
-main()
+len = 0
+ans = 0
+array = prime.each_index{|i|
+      prime[i] = i unless prime[i].zero?
+}.select{|x| !x.zero? }
+array.each_index{|i|
+	count = 0
+	array[i...array.size].inject(0) {|s, x|
+	    s += x
+	    break if s >= max
+	    count += 1
+	    ans, len = s, [len, count].max if !prime[s].zero? && len < count
+	    s
+	 }
+}
+puts ans
